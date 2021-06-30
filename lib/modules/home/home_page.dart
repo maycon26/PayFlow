dd2943337ee03f5f5083c1a3a9ee1c1f1d54a844
Maycon Felipe <felipemaycon26@gmail.com>
2021-06-30 17:27:44 -0300

    Build homepage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,13 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(
+      color: Colors.purple,
+    ),
+    Container(
+      color: Colors.green,
+    )
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Homepage"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(text: "Maycon", style: AppTextStyles.titleBoldBackground),
+                  ]
+                ),
+              ),
+              subtitle: Text("Mantenha suas contas em dia", style: AppTextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.black
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+
+      body: pages[controller.currentPage],
+
+      bottomNavigationBar: Container(
+        height: 90,
+        //color: AppColors.primary,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: (){
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home_outlined,
+                )),
+            GestureDetector(
+              onTap: () {print("cricou");},
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: AppColors.primary,
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+
+            IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.primary,
+              ),),
+          ],
+        ),
       ),
     );
   }
